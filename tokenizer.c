1241d0a1f9c46c33f4cabe093c724c33104c8cbf
Julien Laiti <julien.laiti@gmail.com>
2023-02-27 11:10:49 +0100

    stub for tonekizer

--- tokenizer.c
+++ tokenizer.c
@@ -1,11 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlaiti <jlaiti@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/27 10:48:19 by jlaiti            #+#    #+#             */
+/*   Updated: 2023/02/27 11:06:09 by jlaiti           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+typedef enum e_token_type
+{
+	WORD = 0,
+	SPACE,
+	TAB
+}			t_token_type;
 
 typedef struct s_token
 {
-	void		*type;
-	int		pos;
-	struct	s_token	*next;
-	struct	s_token	*prev;
-}		t_token;
+	t_token_type	type;
+	char			*content;
+	int				content_len;
+	int				pos;
+}			t_token;
+
+typedef	enum e_tokenizer_mode
+{
+	NORMAL = 0,
+	SQ_STRING,
+	DQ_STRING,
+	DONE
+}			t_tok_mode;
+
+typedef struct s_tokenizer
+{
+	char		*input;
+	int			pos;
+	t_tok_mode	mode;
+}			t_tokenizer;
 
 
 void	check_is_word(char **prompt, t_token *token)
