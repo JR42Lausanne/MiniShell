fcbaa2e601952ea4b21dedeb29e27f0c655eaf8b
Guilhem Raux <graux@c1r6s1.42lausanne.ch>
2023-03-01 16:48:03 +0100

    tokenizer as array

--- src/tokenizer_as_array.c
+++ src/tokenizer_as_array.c
@@ -6,7 +6,7 @@
 /*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 16:09:18 by graux             #+#    #+#             */
-/*   Updated: 2023/03/01 16:10:18 by graux            ###   ########.fr       */
+/*   Updated: 2023/03/01 16:47:33 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,22 @@
 
 t_token	**tokenizer_as_array(t_tokenizer *toker)
 {
-	(void) toker;
+	t_token	**array;
+	t_list	*lst;
+	int		i;
+	int		len;
 
-	//TODO implement
-	return (NULL);
+	len = ft_lstsize(toker->tokens);
+	array = malloc((len + 1) * sizeof(t_token *));
+	if (!array)
+		return (NULL);
+	lst = toker->tokens;
+	i = 0;
+	while (lst)
+	{
+		array[i++] = lst->content;
+		lst = lst->next;
+	}
+	array[i] = NULL;
+	return (array);
 }
