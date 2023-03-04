fbc26593d64c8908af1864383b83280c28d5741d
Guilhem Raux <g.raux@icloud.com>
2023-03-04 15:44:59 +0100

    basic space tokens agreg, not done

--- src/tokenize_input.c
+++ src/tokenize_input.c
@@ -6,11 +6,40 @@
 /*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 17:21:59 by graux             #+#    #+#             */
-/*   Updated: 2023/03/03 17:24:49 by graux            ###   ########.fr       */
+/*   Updated: 2023/03/04 15:35:26 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/tokenizer.h"
+
+static t_token	**agregate_spaces(t_token **tokens)
+{
+	t_token	**agreg;
+	int		i;
+	int		j;
+	int		size;
+
+	size = tokens_size(tokens);
+	agreg = malloc((size + 1) * sizeof(t_token *));
+	if (!agreg)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (i < size)
+	{
+		if (!tokens[i + 1] || tokens[i + 1]->type == TOK_SPACE)
+		{
+			tokens[i]->tok_num = j;
+			agreg[j++] = tokens[i];
+			token_destroy(tokens[i + 1]);
+			i += 2;
+		}
+		//TODO if next is not space agregate and stay in same pos
+	}
+	agreg[j] = NULL;
+	free(tokens);
+	return (agreg);
+}
 
 t_token	**tokenize_input(char *input)
 {
@@ -19,6 +48,7 @@ t_token	**tokenize_input(char *input)
 
 	toker = tokenizer_create(input);
 	tokens = tokenizer_parse(toker);
+	tokens = agregate_spaces(tokens);
 	tokenizer_destroy(toker);
 	return (tokens);
 }
