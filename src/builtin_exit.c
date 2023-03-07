fbcddb368d03a581750da806a198c04807002578
Guilhem Raux <graux@c1r6s1.42lausanne.ch>
2023-03-07 13:08:28 +0100

    builtin exit

--- src/builtin_exit.c
+++ src/builtin_exit.c
@@ -6,14 +6,45 @@
 /*   By: jlaiti <jlaiti@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 11:30:08 by jlaiti            #+#    #+#             */
-/*   Updated: 2023/03/07 11:31:51 by jlaiti           ###   ########.fr       */
+/*   Updated: 2023/03/07 13:08:07 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/builtins.h"
+#include "../include/libft.h"
+#include <stdlib.h>
 
+int	check_numeric(char *arg)
+{
+	int	i;
+
+	i = -1;
+	while (arg[++i])
+	{
+		if (!ft_isdigit(arg[i]))
+			return (255);
+	}
+	return (0);
+}
+
+//TODO error handling for invalid inputs
 int	builtin_exit(char **args)
 {
-	(void) args;
-	return (0);
+	int	status;
+
+	printf("exit\n");
+	if (!args)
+		return (1);
+	if (args[1] && args[2])
+		status = 1;
+	else if (args[1])
+	{
+		status = check_numeric(args[1]);
+		if (status == 0)
+			status = ft_atoi(args[1]);
+	}
+	else
+		status = 0;
+	exit(status & 0xFF);
+	return (1);
 }
