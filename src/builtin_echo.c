aed4b2e320337ed87844b2cbea66b186309d7a93
Guilhem Raux <graux@c1r6s1.42lausanne.ch>
2023-03-20 16:52:40 +0100

    fixed dash n option echo

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: jlaiti <jlaiti@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 17:08:45 by jlaiti            #+#    #+#             */
-/*   Updated: 2023/03/20 16:25:39 by graux            ###   ########.fr       */
+/*   Updated: 2023/03/20 16:51:20 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,29 @@
 #include "../include/libft.h"
 #include <stdio.h>
 
+static int	check_n(char *str)
+{
+	int	i;
+	int	status;
+
+	if (!str)
+		return (0);
+	i = -1;
+	status = 0;
+	while (str[++i])
+	{
+		if (i != 0 && str[i] == '-')
+			return (0);
+		else if (str[0] == '-' && str[i] == 'n')
+			status = 1;
+	}
+	return (status);
+}
+
 int	builtin_echo(char **args)
 {
 	int	i;
+	int	dash_n;
 
 	if (!args[1])
 	{
@@ -24,15 +44,19 @@ int	builtin_echo(char **args)
 		return (0);
 	}
 	i = 0;
-	if (ft_strncmp(args[1], "-n", 2) == 0)
+	dash_n = check_n(args[i + 1]);
+	while (dash_n)
+	{
 		i++;
+		dash_n = check_n(args[i + 1]);
+	}
 	while (args[++i])
 	{
 		printf("%s", args[i]);
 		if (args[i + 1])
 			printf(" ");
 	}
-	if (!(ft_strncmp(args[1], "-n", 2) == 0))
+	if (!check_n(args[1]))
 		printf("\n");
 	return (0);
 }
