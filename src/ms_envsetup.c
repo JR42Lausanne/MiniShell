42513b5c5d986d28bac9fdb4901810efab592d95
Guilhem Raux <graux@c1r6s1.42lausanne.ch>
2023-03-21 16:59:06 +0100

    shlvl increase

--- src/ms_envsetup.c
+++ src/ms_envsetup.c
@@ -6,14 +6,38 @@
 /*   By: jlaiti <jlaiti@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 11:44:53 by jlaiti            #+#    #+#             */
-/*   Updated: 2023/03/17 12:38:15 by graux            ###   ########.fr       */
+/*   Updated: 2023/03/21 16:37:56 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 #include "../include/libft.h"
+#include "../include/builtins.h"
 
 char	**g_env;
+
+static void	increase_shlvl(void)
+{
+	char	*shlvl;
+	char	*args[3];
+	char	*temp;
+	int		i_shlvl;
+
+	shlvl = ms_getenv_cont("SHLVL");
+	args[0] = "export";
+	args[2] = NULL;
+	if (!shlvl)
+		args[1] = ft_strjoin("SHLVL=", "1");
+	else
+	{
+		i_shlvl = ft_atoi(shlvl) + 1;
+		temp = ft_itoa(i_shlvl);
+		args[1] = ft_strjoin("SHLVL=", temp);
+		free(temp);
+	}
+	builtin_export(args);
+	free(args[1]);
+}
 
 int	ms_envsetup(char **envp)
 {
@@ -32,5 +56,6 @@ int	ms_envsetup(char **envp)
 		return (1); //TODO free
 	while (i < MAX_ENV)
 		g_env[i++] = NULL;
+	increase_shlvl();
 	return (0);
 }
