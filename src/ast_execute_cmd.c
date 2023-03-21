acc0a4e832a0aaf69aa9bf128de4c6c6f258967a
Guilhem Raux <graux@c1r6s1.42lausanne.ch>
2023-03-21 18:57:45 +0100

    still need to fix invalid cmds ex: /Users

--- src/ast_execute_cmd.c
+++ src/ast_execute_cmd.c
@@ -6,7 +6,7 @@
 /*   By: jlaiti <jlaiti@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 16:04:14 by jlaiti            #+#    #+#             */
-/*   Updated: 2023/03/15 17:14:15 by graux            ###   ########.fr       */
+/*   Updated: 2023/03/21 18:56:47 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 #include "../include/minishell.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 static int	check_relative(char *cmd)
 {
@@ -24,6 +26,14 @@ static int	check_relative(char *cmd)
 	else if (ft_strnstr(cmd, "../", ft_strlen(cmd)))
 		return (1);
 	return (0);
+}
+
+static int	is_regular(char *cmd)
+{
+	struct stat	file_stat;
+
+	stat(cmd, &file_stat);
+	return (!S_ISDIR(file_stat.st_mode));
 }
 
 static char	*get_cmd(char *cmd)
@@ -46,7 +56,7 @@ static char	*get_cmd(char *cmd)
 		tmp = ft_strjoin(path[i], "/");
 		command = ft_strjoin(tmp, cmd);
 		free(tmp);
-		if (access(command, 0) == 0)
+		if (access(command, X_OK) == 0 && is_regular(command))
 			return (command);
 		free(command);
 	}
@@ -62,7 +72,8 @@ void	ast_execute_cmd(t_ast_node *node)
 	cmd_full_path = get_cmd(content->cmd_name);
 	if (!cmd_full_path)
 	{
-		printf("Command not found\n"); //TODO set errno and use perror
+		perror(content->cmd_name);
+		node->exit_status = 127;
 		return ;
 	}
 	node->pid = fork();
