498e324f716a846b29dad056b066e233e352456e
Julien Laiti <julien.laiti@gmail.com>
2023-03-29 16:50:43 +0200

    refactoring builtin cd

--- include/builtins.h
+++ include/builtins.h
@@ -6,19 +6,21 @@
 /*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 16:03:53 by graux             #+#    #+#             */
-/*   Updated: 2023/03/27 16:42:13 by graux            ###   ########.fr       */
+/*   Updated: 2023/03/29 16:25:55 by jlaiti           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
 
-int	builtin_echo(char **args);
-int	builtin_cd(char **args);
-int	builtin_pwd(char **args);
-int	builtin_export(char **args);
-int	builtin_unset(char **args);
-int	builtin_env(char **args);
-int	builtin_exit(char **args);
+int		builtin_echo(char **args);
+int		builtin_cd(char **args);
+int		builtin_pwd(char **args);
+int		builtin_export(char **args);
+void	export_without_args(void);
+int		builtin_unset(char **args);
+int		builtin_env(char **args);
+int		builtin_exit(char **args);
+int		var_name_len(char *var);
 
 #endif
