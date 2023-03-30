a42ef7d99409e797afded5300666ba54cb794fd3
Guilhem Raux <graux@c1r6s1.42lausanne.ch>
2023-03-30 12:33:08 +0200

    change of _ env var after each command execution

--- src/ast_execute.c
+++ src/ast_execute.c
@@ -6,11 +6,12 @@
 /*   By: jlaiti <jlaiti@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 13:22:42 by jlaiti            #+#    #+#             */
-/*   Updated: 2023/03/15 17:13:24 by graux            ###   ########.fr       */
+/*   Updated: 2023/03/30 12:31:38 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/execute.h"
+#include "../include/builtins.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -59,6 +60,29 @@ static void	handle_pipe(t_ast_node *node)
 	*(node->pipe_index) += 2;
 }
 
+static void	export_last_arg(t_ast_node *node)
+{
+	char	*export_args[3];
+	char	**args;
+	int		i;
+
+	export_args[0] = "export";
+	args = NULL;
+	if (node->type == AST_CMD)
+		args = ((t_cmd_cont *)node->content)->args;
+	else if (node->type == AST_BUILTIN)
+		args = ((t_builtin_cont *)node->content)->args;
+	if (!args)
+		return ;
+	i = 0;
+	while (args[i])
+		i++;
+	export_args[1] = ft_strjoin("_=", args[i - 1]);
+	export_args[2] = NULL;
+	builtin_export(export_args);
+	free(export_args[1]);
+}
+
 void	ast_execute(t_ast_node *node)
 {
 	if (!node)
@@ -77,4 +101,5 @@ void	ast_execute(t_ast_node *node)
 		ast_execute_cmd(node);
 	else if (node->type == AST_BUILTIN)
 		ast_execute_built(node);
+	export_last_arg(node);
 }
